ff17d0fb3e5762fa94621cd58d18e5df14fc5e48
Pinxiu Gong <pinxiu@berkeley.edu>
2018-10-24 20:25:58 -0700

    fix

--- gpfood/settings.py
+++ gpfood/settings.py
@@ -123,12 +123,11 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
-# STATICFILES_DIRS = ([
-#     os.path.join(os.path.dirname(BASE_DIR), 'recipe', 'static'),    
-# ])
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'gpfood', 'recipe', 'static')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../recipe/static'),
+)
