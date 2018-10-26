8216b8dad0c4731307017760f5c5f8ec32b06dd0
Pinxiu Gong <pinxiu.gong@avast.com>
2018-10-25 18:39:48 -0700

    add bootstrap templates

--- recipe/views.py
+++ recipe/views.py
@@ -1,5 +1,5 @@
 # from django.http import HttpResponseRedirect
-# from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render
 # from django.urls import reverse
 # from django.utils import timezone
 # from django.views import generic
@@ -37,13 +37,13 @@
 #     template_name = 'recipe/results.html'
 
 
-# def vote(request, question_id):
+def vote(request):
 #     question = get_object_or_404(Question, pk=question_id)
 #     try:
 #         selected_choice = question.choice_set.get(pk=request.POST['choice'])
 #     except (KeyError, Choice.DoesNotExist):
 #         # Redisplay the question voting form.
-#         return render(request, 'recipe/detail.html', {
+    return render(request, 'recipe/base.html')#, {
 #             'question': question,
 #             'error_message': "You didn't select a choice.",
 #         })
